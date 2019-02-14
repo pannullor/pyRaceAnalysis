f9c4b4716ab8ed28893910ae5240a70fa9f88c88
pannullor <pannullor@gmail.com>
2019-02-14 00:59:10 -0500

    working out driver class.

--- racing_reference/api/driver.py
+++ racing_reference/api/driver.py
@@ -9,16 +9,18 @@ class Driver(Scraper):
 
     def __init__(self, name):
         self.name = name
-        self.url = "https://www.racing-reference.info/driver/{}".format(self.name.replace(' ', '_'))
-        self.page = self.fetch_page(self.url)
 
-    def get_tables(self):
-        """
-        simple helper method just return all the tables
-        :return: all the tables
-        """
+        # the active driver page
+        self.page = self.fetch_page("/driver/{}".format(self.name.replace(' ', '_')))
 
-        return self.page.find_all('table')
+        # the drivers name is used in driver data
+        # urls for pages about the driver. The name
+        # is used in the URL as the first 5 of the
+        # last name and first two of the first name
+        split = self.name.split()
+        fname = split[0]
+        lname = split[1]
+        self.driver_key = "{}{}01".format(lname[:5], fname[0:2]).lower()
 
     def driver_info(self, info):
         """
@@ -34,20 +36,31 @@ class Driver(Scraper):
         """
 
         # get the info table
-        text = self.get_tables()[4].find('td').text
+        df = self.get_table(self.page, 4)
+
+        text = df[0][0]
+
+        if info.lower() == 'home':
+            end = text.lower().find('glossary') - 1
+        elif info.lower() == 'born':
+            end = text.lower().find('home') - 1
 
         # starting point is the string we can use to
         # determine where the info bit is at
-        start = text.find(info)
+        start = text.lower().find(info.lower())
 
         # the information is the substring from start
         # to the next line break.
-        extracted = text[start:text.find('\n', start)].replace(info, '')
+        extracted = text[start:end].replace(info, '').strip(": ")
         return extracted
 
     @property
+    def hometown(self):
+        return self.driver_info('Home')
+
+    @property
     def birth_date(self):
-        return self.driver_info('Born: ')
+        return self.driver_info('Born')
 
     @property
     def age(self):
@@ -62,5 +75,68 @@ class Driver(Scraper):
         return difference.years
 
     @property
-    def cup_years_active(self):
-        return self.get_tables()[7].find('tr', {'class': 'tot'}).find('td').text.split()[0]
+    def cup_stats(self):
+        """
+        the drivers cup stats aggregated by year
+        :return:
+        """
+        df = self.get_table(self.page, 7)
+
+        # fill the rank NaN with their average career rank
+        df['Rank'] = df['Rank'].fillna(df['Rank'].mean()).astype(int)
+
+        # return the dataframe
+        return df
+
+    # get the season stats
+    def get_season(self, year):
+        # build the url to the season stats sheet
+
+        stats_url = "/drivdet/{}/{}/W".format(self.driver_key, year)
+
+        # fetch the page to scrape.
+        stats_page = self.fetch_page(stats_url)
+
+        # next get the table dataframe.
+        table = self.get_table(stats_page, 4)
+
+        return table
+
+    # get a drivers career stats at any track
+    def get_track(self, track):
+        """
+        fetch the drivers career statistics for
+        any track the driver has driven at.
+        :param track: the name of the track
+        :return:
+        """
+
+        # track name in url
+        track_key = track.replace(' ', '_')
+
+        # the track page contains a link to
+        # viewing all cup drivers at the track,
+        # that needs to be extracted.
+        track_page = self.fetch_page("/tracks/{}".format(track_key))
+
+        # the table containing the link
+        table = track_page.find_all('table')[6]
+
+        # url that contains track ID
+        url = table.find('a').get('href')
+
+        # pull the track ID out of that url
+        start = len('/trackdet/')
+        track_id = url[start: url.find('/', start+1)]
+
+        # with the track ID extracted, a URL to the
+        # drivers history at the track can be constructed.
+        query_params = {'id': self.driver_key, 'trk': str(track_id), 'series': 'W'}
+        track_history_url = "driverlog"
+
+        # the drivers history at the track page.
+        track_history_page = self.fetch_page(track_history_url, query_params)
+
+        # finally the dataframe of the driver track history
+        df = self.get_table(track_history_page, 4)
+        print(df)
--- racing_reference/scraper.py
+++ racing_reference/scraper.py
@@ -1,24 +1,44 @@
 from bs4 import BeautifulSoup
 import requests
 
+import pandas as pd
+
 
 # race scraper for any race at
 # any track, given that race has
 # actually been run of course.
 class Scraper(object):
-    domain = 'http://racing-reference.info'
+    domain = 'https://racing-reference.info'
 
     # helper method to get the soup
     # object of any url
-    @staticmethod
-    def fetch_page(url):
-        r = requests.get(url)
+    def fetch_page(self, relative_url, query_params=None):
+        url = "{}{}".format(self.domain, relative_url)
+
+        if query_params:
+            r = requests.get(url, params=query_params)
+        else:
+            r = requests.get(url)
 
         # return the soup if a 200 success
         # otherwise raise an exception.
         if r.status_code == 200:
-            soup = BeautifulSoup(r.text, 'html.parser')
+            soup = BeautifulSoup(r.text, 'lxml')
             return soup
         else:
-            raise Exception("Error with fetching url {} -- status code {} ".format(url, r.status_code))
+            raise Exception("Error with fetching url {} -- status code {} ".format(r.url, r.status_code))
+
+    def get_table(self, page, index):
+        """
+        return the HTML at the specified index as
+        a pandas dataframe
+        :param index:
+        :return: table as dataframe
+        """
+
+        table = page.find_all('table')[index]
+
+        df = pd.read_html(str(table))
+
+        return df
 
